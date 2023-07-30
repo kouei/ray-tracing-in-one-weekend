1228ca53c1d2dc8a04e1fb32051028bab6c0eaf5
kouei <kagigokoro@gmail.com>
2023-07-31 02:32:00 +0800

    Add listing 16 to 22

---
+++ archive/listing16/ray-and-normal-direction.cc
@@ -0,0 +1,7 @@
+if (dot(ray_direction, outward_normal) > 0.0) {
+    // ray is inside the sphere
+    ...
+} else {
+    // ray is outside the sphere
+    ...
+}
---
+++ archive/listing17/remember-surface-side.cc
@@ -0,0 +1,10 @@
+bool front_face;
+if (dot(ray_direction, outward_normal) > 0.0) {
+    // ray is inside the sphere
+    normal = -outward_normal;
+    front_face = false;
+} else {
+    // ray is outside the sphere
+    normal = outward_normal;
+    front_face = true;
+}
