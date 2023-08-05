fd3044b6bb862b88bd40376ed6d6794e281eb6c5
kouei <kagigokoro@gmail.com>
2023-08-06 01:55:40 +0800

    Fix sphere.h in src/

--- src/hittable.h
+++ src/hittable.h
@@ -10,10 +10,10 @@ struct hit_record {
   bool front_face;
   uint8_t _padding[7];
 
-  // inline void set_face_normal(const ray &r, const vec3 &outward_normal) {
-  //   front_face = dot(r.direction(), outward_normal) < 0;
-  //   normal = front_face ? outward_normal : -outward_normal;
-  // }
+  inline void set_face_normal(const ray &r, const vec3 &outward_normal) {
+    front_face = dot(r.direction(), outward_normal) < 0;
+    normal = front_face ? outward_normal : -outward_normal;
+  }
 };
 
 class hittable {
