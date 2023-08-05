e92fbad6797cc46f2a76cd96cec30bb6b298b993
kouei <kagigokoro@gmail.com>
2023-08-06 02:37:45 +0800

    Fix warning C5204

--- archive/listing18/hittable.h
+++ archive/listing18/hittable.h
@@ -19,6 +19,7 @@ class hittable {
 public:
   virtual bool hit(const ray &r, double t_min, double t_max,
                    hit_record &rec) const = 0;
+  virtual ~hittable() = default;
 };
 
 #endif
