015caec0c6c121e224455301a8d8a49bc2b36bd2
kouei <kagigokoro@gmail.com>
2023-08-06 03:37:17 +0800

    Add listing 41 to 50

--- archive/listing41/material.h
+++ archive/listing41/material.h
@@ -9,6 +9,7 @@ class material {
 public:
   virtual bool scatter(const ray &r_in, const hit_record &rec,
                        color &attenuation, ray &scattered) const = 0;
+  virtual ~material() = default;
 };
 
 #endif
