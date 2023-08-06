2c47fdead847f1db2eb25b6b237676b0a9633cd5
kouei <kagigokoro@gmail.com>
2023-08-06 22:43:32 +0800

    Add listing 67 to 69

--- archive/listing69/main.cc
+++ archive/listing69/main.cc
@@ -65,7 +65,7 @@ int main() {
   point3 lookfrom(3,3,2);
   point3 lookat(0,0,-1);
   vec3 vup(0,1,0);
-  auto dist_to_focus = (lookfrom - lookat).length();
+  auto dist_to_focus = (lookfrom-lookat).length();
   auto aperture = 2.0;
 
   camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
