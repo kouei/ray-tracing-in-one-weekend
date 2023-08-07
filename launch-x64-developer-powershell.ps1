08d2cc1a19f9052bf077aa73593ade65eb7436fa
kouei <kagigokoro@gmail.com>
2023-08-08 01:14:20 +0800

    Remove launch-x64-developer-powershell.ps1

--- launch-x64-developer-powershell.ps1
+++
@@ -1,5 +0,0 @@
-$currentPath = Get-Location
-$path = Read-Host "Please enter full path to 'Launch-VsDevShell.ps1'"
-& "$path" -Arch arm64 -HostArch amd64
-Set-Location $currentPath
-Import-Module -Force .\init.psm1
