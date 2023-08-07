8f1059210116b95285e426008947ca80afcecdd5
kouei <kagigokoro@gmail.com>
2023-08-08 00:24:22 +0800

    Separate compile command and link command

--- init.psm1
+++ init.psm1
@@ -26,7 +26,9 @@ function Create-DirectoryIfNotExists {
 
 function Build-RayTracer {
     Create-DirectoryIfNotExists -Path bin
-    CL /EHsc /Fo:"bin\" /Fe:"bin\main.exe" /O2 /Wall /wd4711 /wd4710 /wd5045 /wd4514 /wd4820 /wd4100 src\*.cc
+    $compileCommand = 'CL /EHsc /Fo:"bin\" /Fe:"bin\main.exe" /O2 /Wall /wd4711 /wd4710 /wd5045 /wd4514 /wd4820 /wd4100 src\*.cc'
+    $linkCommand = '/link /MACHINE:X86'
+    Invoke-Expression "$compileCommand $linkCommand"
 }
 
 function Render-Output {
