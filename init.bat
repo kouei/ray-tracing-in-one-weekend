a1aecbae52045a33a39dee0b96762c14261b8c19
kouei <kagigokoro@gmail.com>
2023-08-15 02:05:56 +0800

    Add --gpu-architecture=native

--- init.bat
+++ init.bat
@@ -1,3 +1,3 @@
 @echo off
 doskey buildcpu=mkdir .\bin $T CL src\cpu\*.cc /EHsc /Fo:bin\ /Fe:bin\main.exe /O2 /Wall /wd4711 /wd4710 /wd5045 /wd4514 /wd4820 /wd4100
-doskey buildgpu=mkdir .\bin $T nvcc src\gpu\*.cu -o bin\main --generate-code arch=compute_89,code=sm_89
+doskey buildgpu=mkdir .\bin $T nvcc src\gpu\*.cu -o bin\main --gpu-architecture=native
