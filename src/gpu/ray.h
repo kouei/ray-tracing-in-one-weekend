c068aeb4008cf177b48c61d88c7946c17435f168
kouei <kagigokoro@gmail.com>
2023-08-27 13:18:03 +0800

    Add __device__ to ray.h

--- src/gpu/ray.h
+++ src/gpu/ray.h
@@ -1,18 +1,18 @@
 #ifndef RAY_H
 #define RAY_H
 
+#include <cuda_runtime.h>
 #include "vec3.h"
 
 class ray {
 public:
-  ray() {}
-  ray(const point3 &origin, const vec3 &direction)
-      : orig(origin), dir(direction) {}
+  __device__ ray() {}
+  __device__ ray(const point3 &origin, const vec3 &direction): orig(origin), dir(direction) {}
 
-  point3 origin() const { return orig; }
-  vec3 direction() const { return dir; }
+  __device__ point3 origin() const { return orig; }
+  __device__ vec3 direction() const { return dir; }
 
-  point3 at(double t) const { return orig + t * dir; }
+  __device__ point3 at(double t) const { return orig + t * dir; }
 
 public:
   point3 orig;
