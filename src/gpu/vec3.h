fcb711008476c0d423f68787685fbafe4254ddfd
kouei <kagigokoro@gmail.com>
2023-08-27 14:16:46 +0800

    add r(), g(), b() to vec3.h

--- src/gpu/vec3.h
+++ src/gpu/vec3.h
@@ -17,12 +17,15 @@ public:
     return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
   }
 
-  __host__ __device__ vec3() : e{0, 0, 0} {}
+  __host__ __device__ vec3() : e{0.0f, 0.0f, 0.0f} {}
   __host__ __device__ vec3(float e0, float e1, float e2) : e{e0, e1, e2} {}
 
   __host__ __device__ inline float x() const { return e[0]; }
   __host__ __device__ inline float y() const { return e[1]; }
   __host__ __device__ inline float z() const { return e[2]; }
+  __host__ __device__ inline float r() const { return e[0]; }
+  __host__ __device__ inline float g() const { return e[1]; }
+  __host__ __device__ inline float b() const { return e[2]; }
 
   __host__ __device__ inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
   __host__ __device__ inline float operator[](int i) const { return e[i]; }
@@ -42,7 +45,7 @@ public:
     return *this;
   }
 
-  __host__ __device__ inline vec3 &operator/=(const float t) { return *this *= 1 / t; }
+  __host__ __device__ inline vec3 &operator/=(const float t) { return *this *= 1.0f / t; }
 
   __host__ __device__ inline float length() const { return sqrt(length_squared()); }
 
@@ -52,7 +55,7 @@ public:
 
   __host__ __device__ inline bool near_zero() const {
     // Return true if the vector is close to zero in all dimensions.
-    const auto s = 1e-8;
+    const float s = 1e-8;
     return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
   }
 
@@ -88,7 +91,7 @@ __host__ __device__ inline vec3 operator*(float t, const vec3 &v) {
 
 __host__ __device__ inline vec3 operator*(const vec3 &v, float t) { return t * v; }
 
-__host__ __device__ inline vec3 operator/(vec3 v, float t) { return (1 / t) * v; }
+__host__ __device__ inline vec3 operator/(vec3 v, float t) { return (1.0f / t) * v; }
 
 __host__ __device__ inline float dot(const vec3 &u, const vec3 &v) {
   return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
@@ -104,8 +107,8 @@ __host__ __device__ inline vec3 unit_vector(vec3 v) { return v / v.length(); }
 
 __host__ vec3 random_in_unit_sphere() {
   while (true) {
-    auto p = vec3::random(-1, 1);
-    if (p.length_squared() >= 1)
+    auto p = vec3::random(-1.0f, 1.0f);
+    if (p.length_squared() >= 1.0f)
       continue;
     return p;
   }
@@ -115,25 +118,25 @@ __host__ vec3 random_unit_vector() { return unit_vector(random_in_unit_sphere())
 
 __host__ vec3 random_in_hemisphere(const vec3 &normal) {
   vec3 in_unit_sphere = random_in_unit_sphere();
-  if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+  if (dot(in_unit_sphere, normal) > 0.0f) // In the same hemisphere as the normal
     return in_unit_sphere;
   else
     return -in_unit_sphere;
 }
 
-__host__ __device__ inline vec3 reflect(const vec3 &v, const vec3 &n) { return v - 2 * dot(v, n) * n; }
+__host__ __device__ inline vec3 reflect(const vec3 &v, const vec3 &n) { return v - 2.0f * dot(v, n) * n; }
 
 __host__ vec3 refract(const vec3 &uv, const vec3 &n, float etai_over_etat) {
-  auto cos_theta = fmin(dot(-uv, n), 1.0);
+  auto cos_theta = fmin(dot(-uv, n), 1.0f);
   vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
-  vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+  vec3 r_out_parallel = -sqrt(fabs(1.0f - r_out_perp.length_squared())) * n;
   return r_out_perp + r_out_parallel;
 }
 
 __host__ vec3 random_in_unit_disk() {
   while (true) {
-    auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
-    if (p.length_squared() >= 1)
+    auto p = vec3(random_double(-1.0f, 1.0f), random_double(-1.0f, 1.0f), 0.0f);
+    if (p.length_squared() >= 1.0f)
       continue;
     return p;
   }
