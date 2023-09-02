58852f7afe12ce392fa6165c3dc09da517b64a7b
kouei <kagigokoro@gmail.com>
2023-09-02 23:58:32 +0800

    Add *= to vec3

--- src/gpu/vec3.h
+++ src/gpu/vec3.h
@@ -47,6 +47,13 @@ public:
     return *this;
   }
 
+  __host__ __device__ vec3 &operator*=(const vec3 & v) {
+    e[0] *= v.e[0];
+    e[1] *= v.e[1];
+    e[2] *= v.e[2];
+    return *this;
+  }
+
   __host__ __device__ vec3 &operator/=(const float t) { return *this *= 1 / t; }
 
   __host__ __device__ float length() const { return sqrt(length_squared()); }
