c3b640f215c0557d43df6a19235b9fc40bf83413
kouei <kagigokoro@gmail.com>
2023-09-02 22:54:04 +0800

    Migrate rtweekend.h

--- src/gpu/rtweekend.h
+++ src/gpu/rtweekend.h
@@ -1,51 +1,35 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
 
-#include <cmath>
-#include <cstdlib>
-#include <limits>
-#include <memory>
-#include <random>
+#include <curand_kernel.h>
 
-// Usings
+constexpr const float infinity = std::numeric_limits<float>::infinity();
+constexpr const float pi = 3.1415926535897932385f;
 
-using std::make_shared;
-using std::shared_ptr;
-using std::sqrt;
-
-// Constants
-
-const double infinity = std::numeric_limits<double>::infinity();
-const double pi = 3.1415926535897932385;
-
-// Utility Functions
-
-inline double degrees_to_radians(double degrees) {
-  return degrees * pi / 180.0;
+__host__ __device__ inline float degrees_to_radians(float degrees) {
+  return degrees * pi / 180.0f;
 }
 
-inline double random_double() {
-  static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-  static std::mt19937 generator;
-  return distribution(generator);
+// curand_uniform() returns value in (0.0f, 1.0f],
+// Hence write "1.0f - curand_uniform()" instead,
+// to convert it to [0.0f, 1.0f)
+__device__ inline float random_double(curandState * local_rand_state) {
+  return 1.0f - curand_uniform(local_rand_state);
 }
 
-inline double random_double(double min, double max) {
-  // Returns a random real in [min,max).
-  return min + (max - min) * random_double();
+// Returns a random real in [min,max).
+__device__ inline float random_double(float min, float max, curandState * local_rand_state) {
+  return min + (max - min) * random_double(local_rand_state);
 }
 
-inline double clamp(double x, double min, double max) {
-  if (x < min)
+__host__ __device__ inline float clamp(float x, float min, float max) {
+  if (x < min) {
     return min;
-  if (x > max)
+  } else if (x > max) {
     return max;
-  return x;
+  } else {
+    return x;
+  }
 }
-
-// Common Headers
-
-#include "ray.h"
-#include "vec3.h"
 
 #endif
