593721611a7101d8f91506f9a7d6a4eacac11f93
kouei <kagigokoro@gmail.com>
2023-09-03 09:42:38 +0800

    Migrate main.cu

--- src/gpu/main.cu
+++ src/gpu/main.cu
@@ -1,41 +1,101 @@
-#include <chrono>
-#include <iostream>
-#include <curand_kernel.h>
-#include "vec3.h"
-#include "ray.h"
-#include "sphere.h"
-#include "hittable_list.h"
+// Migration Completed
+
 #include "camera.h"
-#include "material.h"
-#include "cuda_utility.h"
 #include "color.h"
+#include "cuda_utility.h"
+#include "hittable_list.h"
+#include "material.h"
+#include "rtweekend.h"
+#include "sphere.h"
 
-constexpr const float TMAX = std::numeric_limits<float>::max();
+#include <chrono>
+#include <curand_kernel.h>
+#include <iostream>
 
-__device__ vec3 ray_color(const ray & r, hittable_ptr_t world, curandState * local_rand_state) {
-    ray cur_ray = r;
-    vec3 cur_attenuation = vec3(1.0f, 1.0f, 1.0f);
-    for(int i = 0; i < 50; i++) {
-        hit_record rec;
-        if (world->hit(cur_ray, 0.001f, TMAX, rec)) {
-            ray scattered;
-            vec3 attenuation;
-            if(rec.mat_ptr->scatter(cur_ray, rec, attenuation, scattered, local_rand_state)) {
-                cur_attenuation *= attenuation;
-                cur_ray = scattered;
-            }
-            else {
-                return vec3(0.0f, 0.0f, 0.0f);
-            }
-        }
-        else {
-            vec3 unit_direction = unit_vector(cur_ray.direction());
-            float t = 0.5f * (unit_direction.y() + 1.0f);
-            vec3 c = (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
-            return cur_attenuation * c;
-        }
+__device__ color ray_color(const ray &r, hittable_ptr_t world, int depth, curandState * local_rand_state) {
+  hit_record rec;
+  ray current_ray = r;
+  color current_attenuation = color(1.0f, 1.0f, 1.0f);
+
+  // If we've exceeded the ray bounce limit, no more light is gathered.
+  for (int i = 0; i < depth; ++i) {
+    if (world->hit(current_ray, 0.001f, infinity, rec)) {
+      ray scattered;
+      color attenuation;
+      if (rec.mat_ptr->scatter(r, rec, attenuation, scattered, local_rand_state)) {
+        current_attenuation *= attenuation;
+        current_ray = scattered;
+      } else {
+        return color(0.0f, 0.0f, 0.0f);
+      }
+    } else {
+      vec3 unit_direction = unit_vector(r.direction());
+      auto t = 0.5f * (unit_direction.y() + 1.0f);
+      auto miss_color = (1.0f - t) * color(1.0f, 1.0f, 1.0f) + t * color(0.5f, 0.7f, 1.0f);
+      return current_attenuation * miss_color;
     }
-    return vec3(0.0f, 0.0f, 0.0f); // exceeded max recursion times
+  }
+
+  return color(0.0f, 0.0f, 0.0f);
+}
+
+__global__ void create_camera(camera * cam, float aspect_ratio) {
+  if (threadIdx.x == 0 && blockIdx.x == 0) {
+    point3 lookfrom(13.0f, 2.0f, 3.0f);
+    point3 lookat(0.0f, 0.0f, 0.0f);
+    vec3 vup(0.0f, 1.0f, 0.0f);
+    float vfov = 20.0f;
+    float aperture = 0.1f;
+    float dist_to_focus = 10.0f;
+
+    new (cam) camera(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus);
+  }
+}
+
+
+__global__ void create_random_scene(hittable_list * world, hittable_ptr_t * objects, curandState * local_rand_state) {
+  if (threadIdx.x == 0 && blockIdx.x == 0) {
+    size_t objects_size = 0;
+    material * ground_material = new lambertian(color(0.5f, 0.5f, 0.5f));
+    objects[objects_size++] = new sphere(point3(0.0f, -1000.0f, 0.0f), 1000.0f, ground_material);
+    
+    for (int a = -11; a < 11; a++) {
+      for (int b = -11; b < 11; b++) {
+        float choose_mat = random_double(local_rand_state);
+        point3 center(a + 0.9f * random_double(local_rand_state), 0.2f, b + 0.9f * random_double(local_rand_state));
+
+        if ((center - point3(4.0f, 0.2f, 0.0f)).length() > 0.9f) {
+          material * sphere_material;
+          if (choose_mat < 0.8f) {
+            // diffuse
+            auto albedo = color::random(local_rand_state) * color::random(local_rand_state);
+            sphere_material = new lambertian(albedo);
+          } else if (choose_mat < 0.95f) {
+            // metal
+            auto albedo = color::random(0.5f, 1.0f, local_rand_state);
+            auto fuzz = random_double(0.0f, 0.5f, local_rand_state);
+            sphere_material = new metal(albedo, fuzz);
+          } else {
+            // glass
+            sphere_material = new dielectric(1.5f);
+          }
+
+          objects[objects_size++] = new sphere(center, 0.2f, sphere_material);
+        }
+      }
+    }
+
+    material * material1 = new dielectric(1.5f);
+    objects[objects_size++] = new sphere(point3(0.0f, 1.0f, 0.0f), 1.0f, material1);
+
+    material * material2 = new lambertian(color(0.4f, 0.2f, 0.1f));
+    objects[objects_size++] = new sphere(point3(-4.0f, 1.0f, 0.0f), 1.0f, material2);
+
+    material * material3 = new metal(color(0.7f, 0.6f, 0.5f), 0.0f);
+    objects[objects_size++] = new sphere(point3(4.0f, 1.0f, 0.0f), 1.0f, material3);
+
+    new (world) hittable_list(objects, objects_size);
+  }
 }
 
 __global__ void render_init(int max_x, int max_y, curandState * rand_state) {
@@ -52,7 +112,7 @@ __global__ void render_init(int max_x, int max_y, curandState * rand_state) {
 
 __global__ void render(vec3 * frame_buffer,
                        int max_x, int max_y, int ns,
-                       camera_ptr_t * cam, hittable_ptr_t * world, curandState *rand_state) {
+                       camera * cam, hittable_ptr_t world, curandState *rand_state, int max_depth) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= max_x) || (j >= max_y)) {
@@ -62,11 +122,12 @@ __global__ void render(vec3 * frame_buffer,
     int pixel_index = j * max_x + i;
     curandState local_rand_state = rand_state[pixel_index];
     vec3 pixel_color(0.0f, 0.0f, 0.0f);
-    for(int s = 0; s < ns; s++) {
-        float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
-        float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
-        ray r = cam[0]->get_ray(u,v);
-        pixel_color += ray_color(r, world[0], &local_rand_state);
+
+    for (int s = 0; s < ns; ++s) {
+      auto u = (i + random_double(&local_rand_state)) / (max_x - 1);
+      auto v = (j + random_double(&local_rand_state)) / (max_y - 1);
+      ray r = cam->get_ray(u, v, &local_rand_state);
+      pixel_color += ray_color(r, world, max_depth, &local_rand_state);
     }
 
     rand_state[pixel_index] = local_rand_state;
@@ -75,102 +136,100 @@ __global__ void render(vec3 * frame_buffer,
     frame_buffer[pixel_index] = pixel_color;
 }
 
-__global__ void create_world(hittable_ptr_t * d_list, hittable_ptr_t * d_world, camera_ptr_t * d_camera) {
-    if (threadIdx.x == 0 && blockIdx.x == 0) {
-        d_list[0] = new sphere(vec3(0.0f, 0.0f,-1.0f), 0.5f, new lambertian(vec3(0.8f, 0.3f, 0.3f)));
-        d_list[1] = new sphere(vec3(0.0f, -100.5f, -1.0f), 100.0f, new lambertian(vec3(0.8f, 0.8f, 0.0f)));
-        d_list[2] = new sphere(vec3(1.0f, 0.0f, -1.0f), 0.5f, new metal(vec3(0.8f, 0.6f, 0.2f), 1.0f));
-        d_list[3] = new sphere(vec3(-1.0f, 0.0f, -1.0f), 0.5f, new metal(vec3(0.8f, 0.8f, 0.8f), 0.3f));
-        d_world[0]  = new hittable_list(d_list, 4);
-        d_camera[0] = new camera();
-    }
-}
-
-__global__ void free_world(hittable_ptr_t * d_list, hittable_ptr_t * d_world, camera_ptr_t * d_camera) {
-    for(int i = 0; i < 4; i++) {
-        delete (reinterpret_cast<sphere *>(d_list[i]))->mat_ptr;
-        delete d_list[i];
-    }
-
-    delete d_world[0];
-    delete d_camera[0];
-}
 
 int main() {
-    int nx = 1200;
-    int ny = 600;
-    int ns = 100;
-    int tx = 8;
-    int ty = 8;
 
-    std::cerr << "Rendering a " << nx << "x" << ny << " image with " << ns << " samples per pixel ";
-    std::cerr << "in " << tx << "x" << ty << " blocks.\n";
+  // Image
 
-    int num_pixels = nx * ny;
-    vec3 * frame_buffer;
-    size_t frame_buffer_size = num_pixels * sizeof(*frame_buffer);
+  const float aspect_ratio = 3.0f / 2.0f;
+  const int image_width = 1200;
+  const int image_height = static_cast<int>(image_width / aspect_ratio);
+  const int samples_per_pixel = 500;
+  const int max_depth = 50;
 
-    // Allocate Frame Buffer
-    checkCudaErrors(cudaMallocManaged(&frame_buffer, frame_buffer_size));
+  const int thread_dim_x = 8;
+  const int thread_dim_y = 8;
 
-    // Allocate Random State
-    curandState * d_rand_state;
-    checkCudaErrors(cudaMalloc(&d_rand_state, num_pixels * sizeof(*d_rand_state)));
+  std::cerr << "Rendering a " << image_width << "x" << image_height << " image with " << samples_per_pixel << " samples per pixel ";
+  std::cerr << "in " << thread_dim_x << "x" << thread_dim_y << " blocks.\n";
 
-    // make our world of hitables & the camera
-    hittable_ptr_t * d_list;
-    checkCudaErrors(cudaMalloc(&d_list, 4 * sizeof(*d_list)));
-    hittable_ptr_t * d_world;
-    checkCudaErrors(cudaMalloc(&d_world, sizeof(*d_world)));
-    camera_ptr_t * d_camera;
-    checkCudaErrors(cudaMalloc(&d_camera, sizeof(*d_camera)));
-    create_world<<<1, 1>>>(d_list, d_world, d_camera);
-    checkCudaErrors(cudaGetLastError());
-    checkCudaErrors(cudaDeviceSynchronize());
+  // Allocate Frame Buffer
+  int num_pixels = image_width * image_height;
+  vec3 * frame_buffer;
+  size_t frame_buffer_size = num_pixels * sizeof(*frame_buffer);
 
-    auto start = std::chrono::high_resolution_clock::now();
+  checkCudaErrors(cudaMallocManaged(&frame_buffer, frame_buffer_size));
 
-    // Render our buffer
+  // Allocate Random State
+  curandState * d_rand_state;
+  checkCudaErrors(cudaMalloc(&d_rand_state, num_pixels * sizeof(*d_rand_state)));
 
-    int block_dim_x = (nx + tx - 1) / tx;
-    int block_dim_y = (ny + ty - 1) / ty;
-    dim3 blocks(block_dim_x, block_dim_y);
-    dim3 threads(tx, ty);
-    
-    render_init<<<blocks, threads>>>(nx, ny, d_rand_state);
-    checkCudaErrors(cudaGetLastError());
-    checkCudaErrors(cudaDeviceSynchronize());
+  int block_dim_x = (image_width + thread_dim_x - 1) / thread_dim_x;
+  int block_dim_y = (image_height + thread_dim_y - 1) / thread_dim_y;
+  dim3 blocks(block_dim_x, block_dim_y);
+  dim3 threads(thread_dim_x, thread_dim_y);
+  
+  render_init<<<blocks, threads>>>(image_width, image_height, d_rand_state);
+  checkCudaErrors(cudaGetLastError());
+  checkCudaErrors(cudaDeviceSynchronize());
 
-    render<<<blocks, threads>>>(frame_buffer, nx, ny,  ns, d_camera, d_world, d_rand_state);
-    checkCudaErrors(cudaGetLastError());
-    checkCudaErrors(cudaDeviceSynchronize());
+  // Camera
+  camera * cam;
+  checkCudaErrors(cudaMalloc(&cam, num_pixels * sizeof(*cam)));
+  create_camera<<<1, 1>>>(cam, aspect_ratio);
+  checkCudaErrors(cudaGetLastError());
+  checkCudaErrors(cudaDeviceSynchronize());
 
-    auto end = std::chrono::high_resolution_clock::now();
+  // World
+  hittable_list * world;
+  checkCudaErrors(cudaMalloc(&world, sizeof(*world)));
 
-    auto timer_in_ms = std::chrono::duration<float, std::milli>(end - start);
-    std::cerr << "Time cost: " << timer_in_ms.count() << "ms.\n";
-    
-    // Output FB as Image
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    for (int j = ny - 1; j >= 0; j--) {
-        for (int i = 0; i < nx; i++) {
-            size_t pixel_index = j * nx + i;
-            int ir = int(255.99 * frame_buffer[pixel_index].r());
-            int ig = int(255.99 * frame_buffer[pixel_index].g());
-            int ib = int(255.99 * frame_buffer[pixel_index].b());
-            std::cout << ir << " " << ig << " " << ib << "\n";
-        }
-    }
+  hittable_ptr_t * objects;
+  checkCudaErrors(cudaMalloc(&objects, 500 * sizeof(*objects)));
 
-    // clean up
-    checkCudaErrors(cudaDeviceSynchronize());
-    free_world<<<1, 1>>>(d_list, d_world, d_camera);
-    checkCudaErrors(cudaGetLastError());
-    checkCudaErrors(cudaFree(d_camera));
-    checkCudaErrors(cudaFree(d_world));
-    checkCudaErrors(cudaFree(d_list));
-    checkCudaErrors(cudaFree(d_rand_state));
-    checkCudaErrors(cudaFree(frame_buffer));
+  create_random_scene<<<1, 1>>>(world, objects, d_rand_state);
+  checkCudaErrors(cudaGetLastError());
+  checkCudaErrors(cudaDeviceSynchronize());
 
-    cudaDeviceReset();
+  auto start = std::chrono::high_resolution_clock::now();
+
+  // Render
+
+  render<<<blocks, threads>>>(frame_buffer, image_width, image_height, samples_per_pixel, cam, world, d_rand_state, max_depth);
+  checkCudaErrors(cudaGetLastError());
+  checkCudaErrors(cudaDeviceSynchronize());
+
+  auto end = std::chrono::high_resolution_clock::now();
+
+  auto timer_in_ms = std::chrono::duration<float, std::milli>(end - start);
+  std::cerr << "Time cost: " << timer_in_ms.count() << "ms.\n";
+
+
+  // Output Frame Buffer as Image
+
+  std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+  for (int j = image_height - 1; j >= 0; j--) {
+      for (int i = 0; i < image_width; i++) {
+          size_t pixel_index = j * image_width + i;
+          int ir = int(255.99f * frame_buffer[pixel_index].x());
+          int ig = int(255.99f * frame_buffer[pixel_index].y());
+          int ib = int(255.99f * frame_buffer[pixel_index].z());
+          std::cout << ir << " " << ig << " " << ib << "\n";
+      }
+  }
+
+  // Clean Up
+  // checkCudaErrors(cudaDeviceSynchronize());
+  // free_world<<<1, 1>>>(d_list, d_world, d_camera);
+  // checkCudaErrors(cudaGetLastError());
+  // checkCudaErrors(cudaFree(d_camera));
+  // checkCudaErrors(cudaFree(d_world));
+  // checkCudaErrors(cudaFree(d_list));
+  // checkCudaErrors(cudaFree(d_rand_state));
+  // checkCudaErrors(cudaFree(frame_buffer));
+
+  // cudaDeviceReset();
+
+  std::cerr << "\nDone.\n";
 }
