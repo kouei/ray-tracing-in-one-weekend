31880a6d2f88e79c6ee77b67d5ab688c305162eb
kouei <kagigokoro@gmail.com>
2023-09-03 18:03:04 +0800

    Implement image2

--- src/gpu/main.cu
+++ src/gpu/main.cu
@@ -1,9 +1,25 @@
+#include "color.h"
 #include "cuda_utility.h"
+#include "ray.h"
 #include "vec3.h"
 #include <chrono>
 #include <iostream>
 
-__global__ void render(vec3 * frame_buffer, int image_width, int image_height) {
+__device__ color ray_color(const ray & r) {
+  vec3 unit_direction = unit_vector(r.direction());
+  float a = 0.5f * (unit_direction.y() + 1.0f);
+  return (1.0f - a) * color(1.0f, 1.0f, 1.0f) + a * color(0.5f, 0.7f, 1.0f);
+}
+
+__global__ void render(
+  vec3 * frame_buffer,
+  int image_width,
+  int image_height,
+  vec3 pixel_delta_u,
+  vec3 pixel_delta_v,
+  point3 pixel00_loc,
+  point3 camera_center) {
+
   int image_x = threadIdx.x + blockIdx.x * blockDim.x;
   int image_y = threadIdx.y + blockIdx.y * blockDim.y;
   if (image_x >= image_width || image_y >= image_height) {
@@ -12,19 +28,23 @@ __global__ void render(vec3 * frame_buffer, int image_width, int image_height) {
 
   int pixel_index = image_y * image_width + image_x;
 
-  float r = float(image_x) / (image_width - 1);
-  float g = float(image_y) / (image_height - 1);
-  float b = 0.0f;
+  auto pixel_center = pixel00_loc + (image_x * pixel_delta_u) + (image_y * pixel_delta_v);
+  auto ray_direction = pixel_center - camera_center;
+  ray r(camera_center, ray_direction);
 
-  frame_buffer[pixel_index] = vec3(r, g, b);
+  color pixel_color = ray_color(r);
+  frame_buffer[pixel_index] = pixel_color;
 }
 
 int main() {
 
   // Image
-
+  float aspect_ratio = 16.0f / 9.0f;
   int image_width = 1920;
-  int image_height = 1080;
+
+  // Calculate the image height, and ensure that it's at least 1.
+  int image_height = static_cast<int>(image_width / aspect_ratio);
+  image_height = (image_height < 1) ? 1 : image_height;
 
   const int samples_per_pixel = 500;
   const int n_thread_x = 16;
@@ -33,6 +53,29 @@ int main() {
   std::clog << "Image Size = " << image_width << "x" << image_height << "\n";
   std::clog << "Samples Per Pixel = " << samples_per_pixel << "\n";
   std::clog << "Block Dim (a x b threads) = " << n_thread_x << "x" << n_thread_y << "\n";
+
+  // Camera
+
+  float focal_length = 1.0f;
+  float viewport_height = 2.0f;
+  float viewport_width = viewport_height * ((float)(image_width) / image_height);
+  point3 camera_center = point3(0.0f, 0.0f, 0.0f);
+
+  // Calculate the vectors across the horizontal and down the vertical viewport edges.
+  vec3 viewport_u = vec3(viewport_width, 0.0f, 0.0f);
+  vec3 viewport_v = vec3(0.0f, -viewport_height, 0.0f);
+
+  // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+  vec3 pixel_delta_u = viewport_u / (float)(image_width);
+  vec3 pixel_delta_v = viewport_v / (float)(image_height);
+
+  // Calculate the location of the upper left pixel.
+  point3 viewport_upper_left = camera_center
+                             - vec3(0.0f, 0.0f, focal_length)
+                             - viewport_u / 2.0f
+                             - viewport_v / 2.0f;
+
+  point3 pixel00_loc = viewport_upper_left + 0.5f * (pixel_delta_u + pixel_delta_v);
 
   // Allocate Frame Buffer
   int n_pixels = image_width * image_height;
@@ -50,7 +93,14 @@ int main() {
 
   auto start = std::chrono::high_resolution_clock::now();
 
-  render<<<blocks, threads>>>(frame_buffer, image_width, image_height);
+  render<<<blocks, threads>>>(
+    frame_buffer,
+    image_width,
+    image_height,
+    pixel_delta_u,
+    pixel_delta_v,
+    pixel00_loc,
+    camera_center);
   checkCudaErrors(cudaGetLastError());
   checkCudaErrors(cudaDeviceSynchronize());
 
