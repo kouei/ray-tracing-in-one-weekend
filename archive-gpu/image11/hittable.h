244689e61bfa1b1866c29d95dbd63c1610036c37
kouei <kagigokoro@gmail.com>
2023-09-07 01:30:35 +0800

    Add image11 to new archive-gpu

---
+++ archive-gpu/image11/hittable.h
@@ -0,0 +1,35 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "interval.h"
+#include "ray.h"
+#include <cuda_runtime.h>
+
+class hit_record {
+public:
+  point3 p;
+  vec3 normal;
+  float t;
+  bool front_face;
+
+  __host__ __device__ void set_face_normal(const ray &r,
+                                           const vec3 &outward_normal) {
+    // Sets the hit record normal vector.
+    // NOTE: the parameter `outward_normal` is assumed to have unit length.
+    // this->normal will always point against the ray.
+
+    this->front_face = dot(r.direction(), outward_normal) < 0.0f;
+    this->normal = this->front_face ? outward_normal : -outward_normal;
+  }
+};
+
+class hittable {
+public:
+  __host__ __device__ virtual ~hittable() {}
+  __host__ __device__ virtual bool
+  hit(const ray &r, interval ray_t, hit_record &rec) const = 0;
+};
+
+typedef hittable *hittable_ptr;
+
+#endif
