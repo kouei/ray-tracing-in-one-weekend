9d640e9d7e110b1140d7a4d37ec800a156b53485
kouei <kagigokoro@gmail.com>
2023-09-07 01:30:01 +0800

    Add image12 to new archive-gpu

---
+++ archive-gpu/image12/interval.h
@@ -0,0 +1,33 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+#include "rtweekend.h"
+
+class interval {
+public:
+  float min, max;
+
+  __host__ __device__ interval()
+      : min(+infinity), max(-infinity) {} // Default interval is empty
+
+  __host__ __device__ interval(float _min, float _max) : min(_min), max(_max) {}
+
+  __host__ __device__ bool contains(float x) const {
+    return min <= x && x <= max;
+  }
+
+  __host__ __device__ bool surrounds(float x) const {
+    return min < x && x < max;
+  }
+
+  __host__ __device__ float clamp(float x) const {
+    return x < min ? x : (max < x ? max : x);
+  }
+
+  static const interval empty, universe;
+};
+
+const static interval empty(+infinity, -infinity);
+const static interval universe(-infinity, +infinity);
+
+#endif
