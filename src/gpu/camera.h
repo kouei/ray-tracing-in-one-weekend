f4457fe6236a3e5fd0dd2596c7420b8fe12e8e6d
kouei <kagigokoro@gmail.com>
2023-09-06 17:16:25 +0800

    Put some code into camera.h

---
+++ src/gpu/camera.h
@@ -0,0 +1,93 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "color.h"
+#include "cuda_utility.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "rtweekend.h"
+#include <cuda_runtime.h>
+
+class camera {
+public:
+  float aspect_ratio;
+  int image_width;
+  int image_height;
+
+// private:
+  point3 center;      // Camera center
+  point3 pixel00_loc; // Location of pixel 0, 0
+  vec3 pixel_delta_u; // Offset to pixel to the right
+  vec3 pixel_delta_v; // Offset to pixel below
+
+public:
+  __global__ friend void initialize(camera *cam);
+  __global__ friend void render(color *frame_buffer, camera *cam,
+                                hittable_list *world);
+};
+
+__global__ void initialize(camera *cam) {
+  if (!(threadIdx.x == 0 && blockIdx.x == 0)) {
+    return;
+  }
+
+  cam->aspect_ratio = 16.0f / 9.0f;
+  cam->image_width = 1920;
+  cam->image_height = static_cast<int>(cam->image_width / cam->aspect_ratio);
+  cam->image_height = (cam->image_height < 1) ? 1 : cam->image_height;
+
+  cam->center = point3(0.0f, 0.0f, 0.0f);
+
+  // Determine viewport dimensions.
+  float focal_length = 1.0f;
+  float viewport_height = 2.0f;
+  float viewport_width =
+      viewport_height * ((float)(cam->image_width) / cam->image_height);
+
+  // Calculate the vectors across the horizontal and down the vertical
+  // viewport edges.
+  vec3 viewport_u = vec3(viewport_width, 0.0f, 0.0f);
+  vec3 viewport_v = vec3(0.0f, -viewport_height, 0.0f);
+
+  // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+  cam->pixel_delta_u = viewport_u / (float)(cam->image_width);
+  cam->pixel_delta_v = viewport_v / (float)(cam->image_height);
+
+  // Calculate the location of the upper left pixel.
+  point3 viewport_upper_left = cam->center - vec3(0.0f, 0.0f, focal_length) -
+                               viewport_u / 2.0f - viewport_v / 2.0f;
+  cam->pixel00_loc =
+      viewport_upper_left + 0.5f * (cam->pixel_delta_u + cam->pixel_delta_v);
+}
+
+__device__ color ray_color(const ray &r, const hittable_list &world) {
+  hit_record rec;
+  if (world.hit(r, interval(0.0f, infinity), rec)) {
+    return 0.5f * (rec.normal + color(1.0f, 1.0f, 1.0f));
+  }
+
+  vec3 unit_direction = unit_vector(r.direction());
+  float a = 0.5f * (unit_direction.y() + 1.0f);
+  return (1.0f - a) * color(1.0f, 1.0f, 1.0f) + a * color(0.5f, 0.7f, 1.0f);
+}
+
+__global__ void render(color *frame_buffer, camera *cam, hittable_list *world) {
+
+  int image_x = threadIdx.x + blockIdx.x * blockDim.x;
+  int image_y = threadIdx.y + blockIdx.y * blockDim.y;
+  if (image_x >= cam->image_width || image_y >= cam->image_height) {
+    return;
+  }
+
+  int pixel_index = image_y * cam->image_width + image_x;
+
+  auto pixel_center = cam->pixel00_loc + (image_x * cam->pixel_delta_u) +
+                      (image_y * cam->pixel_delta_v);
+  auto ray_direction = pixel_center - cam->center;
+  ray r(cam->center, ray_direction);
+
+  color pixel_color = ray_color(r, world[0]);
+  frame_buffer[pixel_index] = pixel_color;
+}
+
+#endif
--- src/gpu/main.cu
+++ src/gpu/main.cu
@@ -1,3 +1,4 @@
+#include "camera.h"
 #include "color.h"
 #include "cuda_utility.h"
 #include "hittable.h"
@@ -11,39 +12,6 @@
 #include <cuda_runtime.h>
 #include <iostream>
 
-__device__ color ray_color(const ray &r, const hittable &world) {
-  hit_record rec;
-  if (world.hit(r, interval(0.0f, infinity), rec)) {
-    return 0.5f * (rec.normal + color(1.0f, 1.0f, 1.0f));
-  }
-
-  vec3 unit_direction = unit_vector(r.direction());
-  float a = 0.5f * (unit_direction.y() + 1.0f);
-  return (1.0f - a) * color(1.0f, 1.0f, 1.0f) + a * color(0.5f, 0.7f, 1.0f);
-}
-
-__global__ void render(color *frame_buffer, int image_width, int image_height,
-                       vec3 pixel_delta_u, vec3 pixel_delta_v,
-                       point3 pixel00_loc, point3 camera_center,
-                       hittable_list *world) {
-
-  int image_x = threadIdx.x + blockIdx.x * blockDim.x;
-  int image_y = threadIdx.y + blockIdx.y * blockDim.y;
-  if (image_x >= image_width || image_y >= image_height) {
-    return;
-  }
-
-  int pixel_index = image_y * image_width + image_x;
-
-  auto pixel_center =
-      pixel00_loc + (image_x * pixel_delta_u) + (image_y * pixel_delta_v);
-  auto ray_direction = pixel_center - camera_center;
-  ray r(camera_center, ray_direction);
-
-  color pixel_color = ray_color(r, world[0]);
-  frame_buffer[pixel_index] = pixel_color;
-}
-
 __global__ void create_world(hittable_list *world) {
   if (threadIdx.x == 0 && blockIdx.x == 0) {
     new (world) hittable_list();
@@ -53,86 +21,59 @@ __global__ void create_world(hittable_list *world) {
   }
 }
 
-hittable_list *create_world_cpu() {
+int main() {
+
+  // World
+
   hittable_list *world;
   checkCudaErrors(cudaMalloc(&world, sizeof(*world)));
   create_world<<<1, 1>>>(world);
   checkCudaErrors(cudaGetLastError());
   checkCudaErrors(cudaDeviceSynchronize());
-  return world;
-}
 
-color *create_frame_buffer_cpu(int image_width, int image_height) {
-  color *frame_buffer;
-  int n_pixels = image_width * image_height;
-  checkCudaErrors(
-      cudaMallocManaged(&frame_buffer, n_pixels * sizeof(*frame_buffer)));
-  return frame_buffer;
-}
+  // Camera
 
-int main() {
+  camera *cam;
+  checkCudaErrors(cudaMallocManaged(&cam, sizeof(*cam)));
+  initialize<<<1, 1>>>(cam);
+  checkCudaErrors(cudaGetLastError());
+  checkCudaErrors(cudaDeviceSynchronize());
 
-  // Image
-  float aspect_ratio = 16.0f / 9.0f;
-  int image_width = 1920;
-
-  // Calculate the image height, and ensure that it's at least 1.
-  int image_height = static_cast<int>(image_width / aspect_ratio);
-  image_height = (image_height < 1) ? 1 : image_height;
+  // std::cout << cam->aspect_ratio << "\n";
+  // std::cout << cam->image_width << "\n";
+  // std::cout << cam->image_height << "\n";
+  // std::cout << cam->center.x() << ' ' << cam->center.y() << ' ' << cam->center.z() << "\n";
+  // std::cout << cam->pixel00_loc.x() << ' ' << cam->pixel00_loc.y() << ' ' << cam->pixel00_loc.z() << "\n";
+  // std::cout << cam->pixel_delta_u.x() << ' ' << cam->pixel_delta_u.y() << ' ' << cam->pixel_delta_u.z() << "\n";
+  // std::cout << cam->pixel_delta_v.x() << ' ' << cam->pixel_delta_v.y() << ' ' << cam->pixel_delta_v.z() << "\n";
 
   const int samples_per_pixel = 500;
   const int n_thread_x = 16;
   const int n_thread_y = 16;
 
-  std::clog << "Image Size = " << image_width << "x" << image_height << "\n";
+  std::clog << "Image Size = " << cam->image_width << "x" << cam->image_height
+            << "\n";
   std::clog << "Samples Per Pixel = " << samples_per_pixel << "\n";
   std::clog << "Block Dim (a x b threads) = " << n_thread_x << "x" << n_thread_y
             << "\n";
 
-  // World
-
-  hittable_list *world = create_world_cpu();
-
-  // Camera
-
-  float focal_length = 1.0f;
-  float viewport_height = 2.0f;
-  float viewport_width =
-      viewport_height * ((float)(image_width) / image_height);
-  point3 camera_center = point3(0.0f, 0.0f, 0.0f);
-
-  // Calculate the vectors across the horizontal and down the vertical viewport
-  // edges.
-  vec3 viewport_u = vec3(viewport_width, 0.0f, 0.0f);
-  vec3 viewport_v = vec3(0.0f, -viewport_height, 0.0f);
-
-  // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-  vec3 pixel_delta_u = viewport_u / (float)(image_width);
-  vec3 pixel_delta_v = viewport_v / (float)(image_height);
-
-  // Calculate the location of the upper left pixel.
-  point3 viewport_upper_left = camera_center - vec3(0.0f, 0.0f, focal_length) -
-                               viewport_u / 2.0f - viewport_v / 2.0f;
-
-  point3 pixel00_loc =
-      viewport_upper_left + 0.5f * (pixel_delta_u + pixel_delta_v);
-
   // Allocate Frame Buffer
 
-  color *frame_buffer = create_frame_buffer_cpu(image_width, image_height);
+  color *frame_buffer;
+  int n_pixels = cam->image_width * cam->image_height;
+  checkCudaErrors(
+      cudaMallocManaged(&frame_buffer, n_pixels * sizeof(*frame_buffer)));
 
   // Render
 
-  int n_block_x = (image_width + n_thread_x - 1) / n_thread_x;
-  int n_block_y = (image_height + n_thread_y - 1) / n_thread_y;
+  int n_block_x = (cam->image_width + n_thread_x - 1) / n_thread_x;
+  int n_block_y = (cam->image_height + n_thread_y - 1) / n_thread_y;
   dim3 blocks(n_block_x, n_block_y);
   dim3 threads(n_thread_x, n_thread_y);
 
   auto start = std::chrono::high_resolution_clock::now();
 
-  render<<<blocks, threads>>>(frame_buffer, image_width, image_height,
-                              pixel_delta_u, pixel_delta_v, pixel00_loc,
-                              camera_center, world);
+  render<<<blocks, threads>>>(frame_buffer, cam, world);
   checkCudaErrors(cudaGetLastError());
   checkCudaErrors(cudaDeviceSynchronize());
 
@@ -143,11 +84,12 @@ int main() {
 
   // Output Image
 
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+  std::cout << "P3\n"
+            << cam->image_width << ' ' << cam->image_height << "\n255\n";
 
-  for (int image_y = 0; image_y < image_height; ++image_y) {
-    for (int image_x = 0; image_x < image_width; ++image_x) {
-      int pixel_index = image_y * image_width + image_x;
+  for (int image_y = 0; image_y < cam->image_height; ++image_y) {
+    for (int image_x = 0; image_x < cam->image_width; ++image_x) {
+      int pixel_index = image_y * cam->image_width + image_x;
       color pixel = frame_buffer[pixel_index];
 
       int ir = static_cast<int>(255.999 * pixel.x());
